092d5ae10f8c10ba7916780fdf19c47bb7341b44
anshikasinghal06 <anshikasinghal089@gmail.com>
2026-07-16 10:39:17 +0530

    Add README and update queries

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,9 +1,5 @@
-/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-Database Load Issues (follow if receiving permission denied when running SQL code below)
-
-NOTE: If you are having issues with permissions. And you get error: 
-
-'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
+/* Database Load Issues (follow if receiving permission denied when running SQL code below)
+NOTE: If you are having issues with permissions. And you get error: 'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
 
 1. Open pgAdmin
 2. In Object Explorer (left-hand pane), navigate to `sql_course` database
@@ -14,15 +10,10 @@ NOTE: If you are having issues with permissions. And you get error:
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
 \copy company_dim FROM 'C:\Users\HP\OneDrive\Desktop\SQL_practice\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
 \copy skills_dim FROM 'C:\Users\HP\OneDrive\Desktop\SQL_practice\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
 \copy job_postings_fact FROM 'C:\Users\HP\OneDrive\Desktop\SQL_practice\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
 \copy skills_job_dim FROM 'C:\Users\HP\OneDrive\Desktop\SQL_practice\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
 */
-
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
